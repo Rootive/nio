5043872035868f34842e574f3c4aff8bad569691
Rootive <815103437@qq.com>
2022-08-12 20:50:19 +0800

    将用编译时注解解决Client

---
+++ src/example/org/rootive/rpc/ClientProcessorTest.java
@@ -0,0 +1,10 @@
+package org.rootive.rpc;
+
+
+public class ClientProcessorTest {
+    @Reference
+    int i;
+
+    @Reference
+    String s = "hello";
+}
--- src/main/org/rootive/rpc/ClientProcessor.java
+++ src/main/org/rootive/rpc/ClientProcessor.java
@@ -1,13 +1,38 @@
 package org.rootive.rpc;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ClientProcessor extends AbstractProcessor {
+    private Messager messager;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment env) {
+        super.init(env);
+        messager = env.getMessager();
+    }
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        return false;
+        for (var annotation : annotations) {
+            log(annotation);
+        }
+        log(roundEnv);
+        return true;
+    }
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> ret = new TreeSet<>();
+        ret.add("org.rootive.rpc.Reference");
+        return ret;
+    }
+
+    private void log(Object obj) {
+        messager.printMessage(Diagnostic.Kind.NOTE, obj.toString());
     }
 }
